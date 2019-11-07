a0d0ddaf400a1d4c01a0d33eab780f3ca026c67a
sunnn <sunnn@DESKTOP-4G7A5TD>
2019-11-07 21:45:41 +0900

    과제용1

--- src/Chapter01/PrimeDetect.java
+++ src/Chapter01/PrimeDetect.java
@@ -1,10 +1,28 @@
 package Chapter01;
 
+import java.util.Scanner;
+
 public class PrimeDetect {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("소수인지 판단할 숫자: ");
 
+		int i = 2;
+		int num = scanner.nextInt();
+		boolean isPrime = true;
+
+		//while 
+			
+		
+		if (isPrime) {
+			System.out.println(num + "은 소수입니다.");
+		} else {
+			System.out.println(num + "은 소수가 아닙니다.");
+
+		}
+		scanner.close();
+	    }	
 	}
 
-}
+
